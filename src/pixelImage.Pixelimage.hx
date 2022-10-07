680826a77f52ca8b4ab8aced266c7086ae7e45df
nanjizal <nanjizal@users.noreply.github.com>
2022-10-07 14:55:03 +0100

    initial experimental commit

---
+++ src/pixelImage.Pixelimage.hx
@@ -0,0 +1,74 @@
+package pixelimage;
+import haxe.io.UInt32Array;
+@:initStruct
+class Pixelimage_ {
+  public var width:  Int;
+  public var height: Int;
+  public var image:  UInt32Array;
+  public function new Pixelimage_( x: Int, y: Int, image: UInt32Array ){
+    this.x = x;
+    this.y = y;
+    this.image = image;
+  }
+}
+abstract Pixelimage( Pixelimage ) from Pixelimage_ {
+    public var width( get, never ): Int;
+    inline function get_width(): Float {
+       return this.width;
+    }
+    public var height( get, never ): Int;
+    inline function get_height(): Float {
+        return this.height;
+    }
+    inline
+    public function new( w: Int, h: Int ){
+       this = ( { width: w, height: h, image: UInt32Array( Std.int( w * h  ) ) }, Pixelimage_ );
+    }
+    inline 
+    function getPosition( x: Int, y: Int ){
+       return Std.int( y * this.width + x );
+    }
+    inline 
+    function alphaLast( argb: Int ): Int {
+        // move two places left and add on alpha
+        return ( argb << 8 ) || ( argb >> 24 & 0xFF );
+    }
+    inline
+    function alphaFrist( rgba: Int ): Int {
+       // use the end alpha and move to front and add on rgb 
+       return ( ( rgba & 0xFF ) << 24 ) || ( rgba >> 8 );
+    }
+    inline
+    public function setARGB( x: Int, y: Int, color: Int ){
+       this.image[ position( x, y ) ] = alphaLast( color );
+    }
+    inline
+    public funcion getARGB( x: Int, y: Int ): Int {
+       return alphaFirst( this.image[ position( x, y ) ] );
+    }
+    inline
+    function pos4( x: Int, y: Int, ?off: Int = 0 ): Int {
+        return Std.int( getPosition( x, y ) * 4 ) + off;
+    }     
+    inline 
+    function view8():UintArray {
+        new haxe.io.UInt8Array( this.image.data.buffer );
+    }    
+    inline
+    public function setIalpha( x: Int, y: Int, alpha: Int ): Int {
+        view8()[ pos4( x, y, 3 ) ] = alpha;    
+    }
+    inline
+    public function getIalpha( x: Int, y: Int ): Int {
+        return view8()[ pos4( x, y, 3 )];   
+    }
+    inline
+    public function setIrgb( x: Int, y: Int, rgb: Int ): Int {
+        var a = getIalph( x, y );
+        this.image[ position( x, y ) ] = rgb << 8 + a;    
+    }
+    inline
+    public function getIrgb( x: Int, y: Int ): Int {
+        return this.image[ position( x, y ) ] >> 8 & 0xFFFFFF;   
+    }
+}     
