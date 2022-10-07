db7dc2e29bd084f413a257da620de33dffe11858
nanjizal <nanjizal@users.noreply.github.com>
2022-10-07 16:25:33 +0100

    it is now rendering something to screen so closer

--- src/pixelImage.Pixelimage.hx
+++ src/pixelImage.Pixelimage.hx
@@ -1,23 +1,23 @@
-package pixelimage;
+package;
 import haxe.io.UInt32Array;
 @:structInit
 class Pixelimage_ {
   public var width:  Int;
   public var height: Int;
   public var image:  UInt32Array;
-  public function new( x: Int, y: Int, image: UInt32Array ){
-    this.x = x;
-    this.y = y;
+  public function new( width: Int, height: Int, image: UInt32Array ){
+    this.width = width;
+    this.height = height;
     this.image = image;
   }
 }
-abstract Pixelimage( Pixelimage ) from Pixelimage_ {
+abstract Pixelimage( Pixelimage_ ) from Pixelimage_ {
     public var width( get, never ): Int;
-    inline function get_width(): Float {
+    inline function get_width(): Int {
        return this.width;
     }
     public var height( get, never ): Int;
-    inline function get_height(): Float {
+    inline function get_height(): Int {
         return this.height;
     }
     inline
@@ -30,18 +30,18 @@ abstract Pixelimage( Pixelimage ) from Pixelimage_ {
         );
     }
     inline 
-    function getPosition( x: Int, y: Int ){
+    function position( x: Int, y: Int ){
        return Std.int( y * this.width + x );
     }
     inline 
     function alphaLast( argb: Int ): Int {
         // move two places left and add on alpha
-        return ( argb << 8 ) || ( argb >> 24 & 0xFF );
+        return ( argb << 8 ) | ( argb >> 24 & 0xFF );
     }
     inline
-    function alphaFrist( rgba: Int ): Int {
+    function alphaFirst( rgba: Int ): Int {
        // use the end alpha and move to front and add on rgb 
-       return ( ( rgba & 0xFF ) << 24 ) || ( rgba >> 8 );
+       return ( ( rgba & 0xFF ) << 24 ) | ( rgba >> 8 );
     }
     inline
     public function setARGB( x: Int, y: Int, color: Int ){
@@ -53,14 +53,15 @@ abstract Pixelimage( Pixelimage ) from Pixelimage_ {
     }
     inline
     function pos4( x: Int, y: Int, ?off: Int = 0 ): Int {
-        return Std.int( getPosition( x, y ) * 4 ) + off;
+        return Std.int( position( x, y ) * 4 ) + off;
     }     
     inline 
-    function view8():UintArray {
-        new haxe.io.UInt8Array( this.image.data.buffer );
+    function view8():js.lib.Uint8Array {
+        var dataimg: js.lib.Uint32Array = cast this.image;
+        return new js.lib.Uint8Array( dataimg.buffer ); // TODO make more generic
     }    
     inline
-    public function setIalpha( x: Int, y: Int, alpha: Int ): Int {
+    public function setIalpha( x: Int, y: Int, alpha: Int ) {
         view8()[ pos4( x, y, 3 ) ] = alpha;    
     }
     inline
@@ -68,8 +69,8 @@ abstract Pixelimage( Pixelimage ) from Pixelimage_ {
         return view8()[ pos4( x, y, 3 )];   
     }
     inline
-    public function setIrgb( x: Int, y: Int, rgb: Int ): Int {
-        var a = getIalph( x, y );
+    public function setIrgb( x: Int, y: Int, rgb: Int ) {
+        var a = getIalpha( x, y );
         this.image[ position( x, y ) ] = rgb << 8 + a;    
     }
     inline
@@ -77,7 +78,7 @@ abstract Pixelimage( Pixelimage ) from Pixelimage_ {
         return this.image[ position( x, y ) ] >> 8 & 0xFFFFFF;   
     }
     inline
-    public function fillRect( x: Int, y: Int, w: Int, h: Int, color: Color ) {
+    public function fillRect( x: Int, y: Int, w: Int, h: Int, color: Int ) {
        var p = x;
        var q = y;
        var maxX = x + w;
@@ -94,8 +95,7 @@ abstract Pixelimage( Pixelimage ) from Pixelimage_ {
     #if js
     inline
     public function drawToContext( ctx: js.html.CanvasRenderingContext2D, x: Int, y: Int  ){
-        var d: js.Lib.UInt8Array = cast view8();
-        var data = new js.lib.Uint8ClampedArray( d.buffer );
+        var data = new js.lib.Uint8ClampedArray( view8().buffer );
         var imageData = new js.html.ImageData( data, this.width, this.height );
         ctx.putImageData( imageData, x, y);
     }
@@ -103,7 +103,8 @@ abstract Pixelimage( Pixelimage ) from Pixelimage_ {
     public function drawFromContext( ctx: js.html.CanvasRenderingContext2D, x: Int, y: Int ){
         var imageData = ctx.getImageData( x, y, this.width, this.height);
         var data = imageData.data;
-        this.image = new haxe.io.UInt32Array( data.buffer );
+        var temp = new js.lib.Uint32Array( data.buffer );
+        this.image = cast temp;
     }
     #end
 }     
