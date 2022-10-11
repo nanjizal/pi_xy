5899e88e82ca7a3028326db39800bffd3fecc0dc
nanjizal <nanjizal@users.noreply.github.com>
2022-10-11 22:44:27 +0100

    Fixed the gradient triangle :)

--- src/pixelImage.Pixelimage.hx
+++ src/pixelImage.Pixelimage.hx
@@ -27,9 +27,9 @@ class Main {
         var p = new Pixelimage( 1024, 768 );
         p.fillRect( 100, 100, 300, 200, 0xff4211bc );
         p.fillTri( 100, 100, 200, 120, 150, 300, 0xffF000c0 );
-        p.fillGradTri( 100, 100, 0xFFFF0000
-                     , 500, 300, 0xFF1ECB24
-                     , 300, 500, 0xFFE9D604 );
+        p.fillGradTri( 100, 100, 0xffff0000
+                     , 500, 300, 0xFF00ff00
+                     , 300, 500, 0xFF0000ff);
         p.fillRegPoly( 300, 300, 90, 200, 0xFF00Ffcc );// circles and ellipses
         p.drawToContext( g.me, 0, 0 );
         p.drawFromContext( g.me, 0, 0 );
@@ -38,6 +38,7 @@ class Main {
 }
 
 */
+package;
 import haxe.io.UInt32Array;
 final isLittleEndian = (() -> {
     final a8 = new js.lib.Uint8Array(4);
@@ -228,7 +229,8 @@ abstract Pixelimage( Pixelimage_ ) from Pixelimage_ {
         for( x in boundIterator3( ax, bx, cx ) ){
             var sxx = sx*x;
             var txx = tx*x;
-            for( y in boundIterator3( ay, by, cx ) ){
+            for( y in boundIterator3( ay, by, cy ) ){
+                
                 if( triCalc( x, y, s0, t0, sxx, sy, txx, ty, A ) ) setARGB( x, y, color );
             }
         }
@@ -243,83 +245,37 @@ abstract Pixelimage( Pixelimage_ ) from Pixelimage_ {
         var t = t0 + txx + ty*y;
         return ( s <= 0 || t <= 0 )? false: (s + t) < A;
     }
-    inline public
+    public inline
     function fillGradTri( ax: Float, ay: Float, colA: Int
                         , bx: Float, by: Float, colB: Int
-                        , cx: Float, cy: Float, colC: Int ){
-        var aA = ( colA >> 24 ) & 0xFF;
-        var rA = ( colA >> 16 ) & 0xFF;
-        var gA = ( colA >> 8 ) & 0xFF;
-        var bA = colA & 0xFF;
-
-        var aB = ( colB >> 24 ) & 0xFF;
-        var rB = ( colB >> 16 ) & 0xFF;
-        var gB = ( colB >> 8 ) & 0xFF;
-        var bB = colB & 0xFF;
-    
+                        ,  cx: Float, cy: Float, colC: Int ){
+        var aA = ( colB >> 24 ) & 0xFF;
+        var rA = ( colB >> 16 ) & 0xFF;
+        var gA = ( colB >> 8 ) & 0xFF;
+        var bA = colB & 0xFF;
+        var aB = ( colA >> 24 ) & 0xFF;
+        var rB = ( colA >> 16 ) & 0xFF;
+        var gB = ( colA >> 8 ) & 0xFF;
+        var bB = colA & 0xFF;
         var aC = ( colC >> 24 ) & 0xFF;
         var rC = ( colC >> 16 ) & 0xFF;
         var gC = ( colC >> 8 ) & 0xFF;
         var bC = colC & 0xFF;
-
-        var v0x = bx - ax;                    
-        var v0y = by - ay;
-        var v1x = cx - ax;
-        var v1y = cy - ay;
-        var d00 = dot( v0x, v0y, v0x, v0y );
-        var d01 = dot( v0x, v0y, v1x, v1y );
-        var d11 = dot( v1x, v1y, v1x, v1y );
-        for( x in boundIterator3( ax, bx, cx ) ){
-            var v2x = x-ax;// does not change every y.
-            for( y in boundIterator3( ay, by, cy) ){
-                barycentricWeightColor( x, y
-                                      , ax, ay
-                                      , v0x, v0y, v1x, v1y, v2x, d00, d01, d11
-                                      , aA, rA, gA, bA
-                                      , aB, rB, gB, gB
-                                      , aC, rC, gC, gC );
-            }
-        }
-    }
-    // second attempt
-    public inline
-    function barycentricGradient( ax: Float, ay: Float, colA: Int
-                                , bx: Float, by: Float, colB: Int
-                                , cx: Float, cy: Float, colC: Int ){
-        var aA = ( colA >> 24 ) & 0xFF;
-        var rA = ( colA >> 16 ) & 0xFF;
-        var gA = ( colA >> 8 ) & 0xFF;
-        var bA = colA & 0xFF;
-
-        var aB = ( colB >> 24 ) & 0xFF;
-        var rB = ( colB >> 16 ) & 0xFF;
-        var gB = ( colB >> 8 ) & 0xFF;
-        var bB = colB & 0xFF;
-    
-        var aC = ( colC >> 24 ) & 0xFF;
-        var rC = ( colC >> 16 ) & 0xFF;
-        var gC = ( colC >> 8 ) & 0xFF;
-        var bC = colC & 0xFF;
-
-        // a = V1, b = V2, c = V3 
-        // before loop calculate
-        var bax = bx - ax; //sideVec1 = V2 - V1
-        var bay = by - ay;
-        var cax = cx - ax; //sideVec2 = V3 - V1  
-        var cay = cy - ay;
- 
-        // before loop calculate
-        var dot11 = dotSame( bax, bay );
-        var dot12 = dot( bax, bay, cax, cay );
-        var dot22 = dotSame( cax, cay );
+        var bcx = bx - cx;
+        var bcy = by - cy;
+        var acx = ax - cx; 
+        var acy = ay - cy;
+        // Had to re-arrange algorithm to work so dot names may not quite make sense.
+        var dot11 = dotSame( bcx, bcy );
+        var dot12 = dot( bcx, bcy, acx, acy );
+        var dot22 = dotSame( acx, acy );
         var denom1 = 1/( dot11 * dot22 - dot12 * dot12 );
-
-        for( px in boundIterator3( ax, bx, cx ) ){
-            var pax = px - ax; //sideVec3 = P - V1;  can be done in x loop
-            for( py in boundIterator3( bx, by, cy ) ){
-                var pay = py - ay; // has to be done in y loop
-                var dot31 = dot( pax, pay, bax, bay );
-                var dot32 = dot( pax, pay, cax, cay );
+        for( px in boundIterator3( cx, bx, ax ) ){
+            var pcx = px - cx;
+            for( py in boundIterator3( cy, by, ay ) ){
+                var pcy = py - cy;
+                var dot31 = dot( pcx, pcy, bcx, bcy );
+                var dot32 = dot( pcx, pcy, acx, acy );
                 var ratioA = (dot22 * dot31 - dot12 * dot32) * denom1;
                 var ratioB = (dot11 * dot32 - dot12 * dot31) * denom1;
                 var ratioC = 1.0 - ratioB - ratioA;
@@ -329,38 +285,9 @@ abstract Pixelimage( Pixelimage_ ) from Pixelimage_ {
                     var g = boundChannel( gA*ratioA + gB*ratioB + gC*ratioC );
                     var b = boundChannel( bA*ratioA + bB*ratioB + bC*ratioC );
                     // re-ordered for canvas abgr
-                    this.image[ position( px, py ) ] = a << 24 | b << 16 | g << 8 | r;
+                    this.image[ position( px, py ) ] = ( this.isLittle )? a << 24 | b << 16 | g << 8 | r: a << 24 | r << 16 | g << 8 | b;
                 }
             }
-        }
-    }
-    inline
-    function barycentricWeightColor( x: Int, y: Int
-                              , ax: Float, ay: Float
-                              , v0x: Float, v0y: Float, v1x: Float, v1y: Float, v2x: Float, d00: Float, d01: Float, d11: Float
-                              , aA: Int, rA: Int, gA: Int, bA: Int
-                              , aB: Int, rB: Int, gB: Int, bB: Int
-                              , aC: Int, rC: Int, gC: Int, bC: Int ): Null<Int> {
-        var v2y = y-ay;
-        var d20 = dot( v2x, v2y, v0x, v0y );
-        var d21 = dot( v2x, v2y, v1x, v1y );
-        var invDenom = 1.0 / (d00 * d11 - d01 * d01);
-        var wa = (d11 * d20 - d01 * d21) * invDenom;
-        var wb = (d00 * d21 - d01 * d20) * invDenom;
-        var wc = 1.0 - wa - wb;                                      
-        return if( wa >= 0 && wb >= 0 && wc >= 0 ){
-            var a = boundChannel( aA*wa + aB*wb + aC*wc );
-            var r = boundChannel( rA*wa + rB*wb + rC*wc );
-            var g = boundChannel( gA*wa + gB*wb + gC*wc );
-            var b = boundChannel( bA*wa + bB*wb + bC*wc );
-            // output as canvas color, so order changed to abgr
-            var col: Int = a << 24 | b << 16 | g << 8 | r;
-            // direct set.. not ideal..
-            // set to red for now!
-            this.image[ position( x, y ) ] = col;
-            col;
-        } else {
-            null;
         }
     }
     inline function boundChannel( f: Float ): Int {
@@ -370,8 +297,8 @@ abstract Pixelimage( Pixelimage_ ) from Pixelimage_ {
         return i;
     }
     inline public
-    function fillPoly( cx: Float, cy: Float
-                     , rx: Float, ry: Float
+    function fillRegPoly( cx:    Float, cy: Float
+                     , rx:    Float, ry: Float
                      , color: Int, ?sides: Int = 36 ){
         var theta = 2*Math.PI/36 +0.001; // slight offset to reduce overlap artifacts.
         // can be solved by including edges .. not implemented yet
@@ -387,9 +314,9 @@ abstract Pixelimage( Pixelimage_ ) from Pixelimage_ {
     }
     inline public
     function distanceSquarePointToSegment( x:  Float, y: Float
-                                                , x1: Float, y1:Float
-                                                , x2: Float, y2:Float
-                                                ): Float
+                                         , x1: Float, y1:Float
+                                         , x2: Float, y2:Float
+                                         ):    Float
     {
         var p1_p2_squareLength = (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1);
         var dotProduct = ((x - x1)*(x2 - x1) + (y - y1)*(y2 - y1)) / p1_p2_squareLength;
@@ -428,5 +355,4 @@ abstract Pixelimage( Pixelimage_ ) from Pixelimage_ {
 // https://gamedev.stackexchange.com/questions/23743/whats-the-most-efficient-way-to-find-barycentric-coordinates    
 // https://stackoverflow.com/questions/39213661/canvas-using-uint32array-wrong-colors-are-being-rendered
 // https://stackoverflow.com/questions/26513712/algorithm-for-coloring-a-triangle-by-vertex-color
-
           
