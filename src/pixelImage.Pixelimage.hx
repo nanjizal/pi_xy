a4499adc2b0b2deb055356fc473490633dfc0683
nanjizal <nanjizal@users.noreply.github.com>
2022-10-14 13:12:32 +0100

    add functionality for rotating regular polygons.

--- src/pixelImage.Pixelimage.hx
+++ src/pixelImage.Pixelimage.hx
@@ -450,20 +450,54 @@ abstract Pixelimage( Pixelimage_ ) from Pixelimage_ {
         if( printSides ) trace( noSides );
         fillRegPoly( cx, cy, rx, ry, color, noSides );
     }
+    // phi controls rotation
+    // cornerUp is to rotate the structure before phi, so top is flat.
     inline public
-    function fillRegPoly( cx: Float, cy: Float
-                     , rx: Float, ry: Float
-                     , color: Int, ?sides: Int = 36 ){
-        var theta = 2*Math.PI/36 +0.001; // slight offset to reduce overlap artifacts.
+    function fillRegPoly( cx: Float,  cy: Float
+                        , rx: Float,  ry: Float
+                        , color: Int, ?phi: Float = 0.
+                        , ?sides: Int = 36, cornerUp: Bool = true ){
+        var theta = 2*Math.PI/sides +0.001; // slight offset to reduce overlap artifacts!! very bad
+        var omega = if( cornerUp ){
+             0.;
+        } else {
+            // allow top the be flat
+            if( Std.int( sides/2) == sides/2 ){
+                Math.PI/sides;
+            } else {
+                0.5*Math.PI/sides;
+            }
+        }
         // can be solved by including edges .. not implemented yet
-        var lastX = cx + rx*Math.cos( sides*theta );
-        var lastY = cy + ry*Math.sin( sides*theta );
-        for( i in 0...sides+1 ){
-            var nextX = cx + rx*Math.cos( i*theta );
-            var nextY = cy + ry*Math.sin( i*theta );
-            fillTri( cx, cy, lastX, lastY, nextX, nextY, color );
-            lastX = nextX;
-            lastY = nextY;
+        var lastX: Float = 0.;
+        var lastY: Float = 0.;
+        if( phi != 0 ){
+            lastX = rx * Math.cos( sides*theta  + omega) * Math.cos( phi ) - ry * Math.sin( sides*theta + omega ) * Math.sin( phi ) + cx;
+            lastY = rx * Math.cos( sides*theta  + omega ) * Math.sin( phi ) + ry * Math.sin( sides*theta + omega ) * Math.cos( phi ) + cy;
+        } else {
+            lastX = cx + rx*Math.cos( sides*theta + omega );
+            lastY = cy + ry*Math.sin( sides*theta + omega );
+        }
+        if( phi != 0 ){
+            var cphi = Math.cos( phi );
+            var sphi = Math.sin( phi );
+            for( i in 0...sides+1 ){
+                var stheta = Math.sin( i*theta + omega );
+                var ctheta = Math.cos( i*theta + omega );
+                var nextX = rx * ctheta * cphi - ry * stheta * sphi + cx;
+                var nextY = rx * ctheta * sphi + ry * stheta * cphi + cy;
+                fillTri( cx, cy, lastX, lastY, nextX, nextY, color );
+                lastX = nextX;
+                lastY = nextY;
+            }
+        } else { 
+            for( i in 0...sides+1 ){
+                var nextX = cx + rx*Math.cos( i*theta + omega );
+                var nextY = cy + ry*Math.sin( i*theta + omega );
+                fillTri( cx, cy, lastX, lastY, nextX, nextY, color );
+                lastX = nextX;
+                lastY = nextY;
+            }
         }
     }
     inline public
