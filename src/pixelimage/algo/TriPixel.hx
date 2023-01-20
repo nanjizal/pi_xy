5bc2b05e7dc146502f63173ac836ed3c45daf620
nanjizal <nitsuj@nanjizal.net>
2023-01-20 12:46:45 +0000

    add possible code for mixing multiple textures on a triangle

--- src/pixelimage/algo/TriPixel.hx
+++ src/pixelimage/algo/TriPixel.hx
@@ -264,6 +264,81 @@ import pixelimage.algo.HitTri;
         }   
     }
 
+// EXPERIMENTAL !!!  work in progress
+inline
+function uvTriangleTexture3( pixelimage: Pixelimage
+                    , textureA: Pixelimage, winA: RectangleWindow
+                    , textureB: Pixelimage, winB: RectangleWindow
+                    , textureC: Pixelimage, winC: RectangleWindow
+                    , ax: Float, ay: Float, au: Float, av: Float
+                    , bx: Float, by: Float, bu: Float, bv: Float
+                    , cx: Float, cy: Float, cu: Float, cv: Float
+                    , hasHit: Bool = false ): Null<HitTri>{
+    var bcx = bx - cx;
+    var bcy = by - cy;
+    var acx = ax - cx; 
+    var acy = ay - cy;
+    // Had to re-arrange algorithm to work so dot names may not quite make sense.
+    var dot11 = dotSame( bcx, bcy );
+    var dot12 = dot( bcx, bcy, acx, acy );
+    var dot22 = dotSame( acx, acy );
+    var denom1 = 1/( dot11 * dot22 - dot12 * dot12 );
+    var colA: Float = 0.;
+    var colB: Float = 0.;
+    var colC: Float = 0.;
+    var u: Int = 0;
+    var v: Int = 0;
+    var col: Int;
+    for( px in boundIterator3( cx, bx, ax ) ){
+        var pcx = px - cx;
+        for( py in boundIterator3( cy, by, ay ) ){
+            var pcy = py - cy;
+            var dot31  = dot( pcx, pcy, bcx, bcy );
+            var dot32  = dot( pcx, pcy, acx, acy );
+            var ratioA = (dot22 * dot31 - dot12 * dot32) * denom1;
+            var ratioB = (dot11 * dot32 - dot12 * dot31) * denom1;
+            var ratioC = 1.0 - ratioB - ratioA;
+            if( ratioA >= 0 && ratioB >= 0 && ratioC >= 0 ){
+                u = au*ratioA + bu*ratioB + cu*ratioC;
+                v = av*ratioA + bv*ratioB + cv*ratioC;
+                x = Std.int( u*winA.width + winA.x );
+                y = Std.int( v*winA.height + winA.y );
+                colA = textureaA.getARGB( x, y );
+                x = Std.int( u*winB.width + winB.x );
+                y = Std.int( v*winB.height + winB.y );
+                colB = textureaB.getARGB( x, y );
+                x = Std.int( u*winC.width + winC.x );
+                y = Std.int( v*winC.height + winC.y );
+                colC = textureaC.getARGB( x, y );
+                // need to check the right a, b, c setup
+                var aA = ( colB >> 24 ) & 0xFF;
+                var rA = ( colB >> 16 ) & 0xFF;
+                var gA = ( colB >> 8 ) & 0xFF;
+                var bA = colB & 0xFF;
+                var aB = ( colA >> 24 ) & 0xFF;
+                var rB = ( colA >> 16 ) & 0xFF;
+                var gB = ( colA >> 8 ) & 0xFF;
+                var bB = colA & 0xFF;
+                var aC = ( colC >> 24 ) & 0xFF;
+                var rC = ( colC >> 16 ) & 0xFF;
+                var gC = ( colC >> 8 ) & 0xFF;
+                var bC = colC & 0xFF;
+                var a = PixelChannel.boundChannel( aA*ratioA + aB*ratioB + aC*ratioC );
+                var r = PixelChannel.boundChannel( rA*ratioA + rB*ratioB + rC*ratioC );
+                var g = PixelChannel.boundChannel( gA*ratioA + gB*ratioB + gC*ratioC );
+                var b = PixelChannel.boundChannel( bA*ratioA + bB*ratioB + bC*ratioC );
+                pixelimage.set_argbPixel( a, r, g, b, pixelimage.position( px, py ) );
+            }
+        }
+    }
+    return if( hasHit == false ){
+        var v: HitTri = { ax: ax, ay: ay, bx: bx, by: by, cx: cx, cy: cy };
+        v;
+    } else {
+        null;
+    }   
+}
+
 class TriPixel{
     /**
        <font color="LightPink" font-weight:"Bold">fillTriUnsafe</font> module level field
